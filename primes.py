6425d564f8ee646e7c8763afb5a5712491669c17
Luca Boccardo <luca.boccardo@kcl.ac.uk>
2022-10-26 23:26:48 +0100

    Finish primes.py code

--- primes.py
+++ primes.py
@@ -1,6 +1,26 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
 def primes(number_of_primes):
     list = []
+    i = 1
+
+    if number_of_primes <= 0:  # Good error check
+        raise (ValueError("This number is out of range!"))
+
+    for x in range(0, number_of_primes):
+        prime_found = False
+
+        while (
+            prime_found == False
+        ):  # Can be a bit confusing constantly changing this variable
+            i += 1
+            prime_found = True
+            for x in range(2, int(i**0.5) + 1):
+                if i % x == 0:
+                    prime_found = False
+                    break
+        list.append(i)
+
     return list
